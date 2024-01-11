dbc8c353890498e28248525b93a5f89ace3aada8
oduwole <segxy2708@yahoo.com>
2024-01-11 09:33:08 +0100

    configdto

--- PallyWad.Domain/Dto/ConfigDto.cs
+++ PallyWad.Domain/Dto/ConfigDto.cs
@@ -10,6 +10,7 @@ namespace PallyWad.Domain.Dto
     {
         public string configname { get; set; }
         public string username { get; set; }
+        public string mailfrom { get; set; }
         public string password { get; set; }
         public string smtp { get; set; }
         public int port { get; set; }
